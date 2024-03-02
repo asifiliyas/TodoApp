91508cc274981f57a76b7b8fd4b4ae4d47fd57e9
Asif Iliyas <asifiliyas1997@gmail.com>
2024-03-02 20:47:10 +0530

    Heading changed

--- src/components/AppName.jsx
+++ src/components/AppName.jsx
@@ -1,5 +1,5 @@
 import styles from "./AppName.module.css";
 function AppName() {
-  return <h1 className={styles.todoHeading}>TODO App</h1>;
+  return <h1 className={styles.todoHeading}>TODO Application</h1>;
 }
 export default AppName;
