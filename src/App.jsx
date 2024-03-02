db7243f7a2b8c62a7539e6de8a2029c8f832560f
Asif Iliyas <asifiliyas1997@gmail.com>
2024-03-02 19:11:20 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import AddTodo from "./components/AddTodo";
+import AppName from "./components/AppName";
+import "./App.css";
+import TodoItems from "./components/TodoItems";
+import { useState } from "react";
+import WelcomeMessage from "./components/WelcomeMeassage";
+function App() {
+  const [todoItems, setTodoItems] = useState([]);
+
+  const handleNewItem = (itemName, itemDueDate) => {
+    const newTodoItems = [
+      ...todoItems,
+      { name: itemName, duedate: itemDueDate },
+    ];
+    setTodoItems(newTodoItems);
+  };
+  const handleDeleteItem = (todoItemName) => {
+    const newTodoItems=todoItems.filter(item=>item.name!=todoItemName)
+    setTodoItems(newTodoItems);
+  };
+  return (
+    <center className="todo-container">
+      <AppName />
+      <AddTodo onNewItem={handleNewItem} />
+      {todoItems.length === 0 && <WelcomeMessage></WelcomeMessage>}
+      <TodoItems
+        todoItems={todoItems}
+        onDeleteClick={handleDeleteItem}
+      ></TodoItems>
+    </center>
+  );
+}
+
+export default App;
